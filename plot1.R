f8c15579e1372262b088680bec97a06ad46afb22
abhi-ab <100691253+abhi-ab@users.noreply.github.com>
2022-03-02 22:24:04 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,8 @@
+#View(exdata_data_household_power_consumption)
 
 dataFile <- "./data/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+subDataSet <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #str(subSetData)
 globalActivePower <- as.numeric(subSetData$Global_active_power)
